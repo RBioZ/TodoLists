81661e0b93191b7e027ba4eb86968a04d835e64e
RBioZ <jonatha_rihan@hotmail.com>
2020-05-14 23:45:40 -0300

    fist day

--- components/AddListModal.js
+++ components/AddListModal.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import {View,Text,StyleSheet,KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import {View,Text,StyleSheet,KeyboardAvoidingView, TouchableOpacity,TextInput} from 'react-native';
 import {AntDesign} from '@expo/vector-icons'
 import colors from '../Colors'
 import Constants from 'expo-constants';
 
 export default class AddListModal extends React.Component{
+
+    backgroundColors = ["#5CD859", "#24A6D9", "#595BD9", "#8022D9", "#D159D8", "#D85963", "#D88559"]
+
+    state ={
+        name:"",
+        color: this.backgroundColors[0]
+    }
+
+    createTodo = () => {
+        const {name,color} = this.state
+
+        tempData.push({
+            name,
+            color,
+            todos:[]
+        });
+
+        this.setState({name:''});
+        this.props.closeModal();
+    }
+
+    renderColors(){
+        return this.backgroundColors.map(color => {
+            return(
+                <TouchableOpacity 
+                    key={color} 
+                    style = {[styles.colorSelect,{backgroundColor:color}]} 
+                    onPress={() => this.setState({color})} 
+                />
+            )
+        })
+    }
+
     render(){
         return(
-            <KeyboardAvoidingView style={styles.container} behavior="padding">
-                <TouchableOpacity style={{position:'absolute', top:15, right:15}}>
+            <View style={styles.container} behavior="padding">
+                <TouchableOpacity style={{position:'absolute', top:15, right:15}} onPress={this.props.closeModal}>
                     <AntDesign name='close' size={24} color={colors.black}/>
                 </TouchableOpacity>
-            </KeyboardAvoidingView>
+                <View style={{alignSelf:'stretch', marginHorizontal:32}}>
+                    <Text style={styles.title}>
+                        Create Todo List
+                    </Text>
+
+                    <TextInput
+                    style={styles.input}
+                    placeholder='List Name?'
+                    onChangeText={text => this.setState({name: text})}
+                    />
+
+                    <View style={{flexDirection:'row',justifyContent:'space-between', marginTop:20}}>
+                        {this.renderColors()}
+                    </View>
+
+                    <TouchableOpacity style={[styles.create, {backgroundColor:this.state.color}]} onPress={this.createTodo}>
+                        <Text style={{color:colors.white,fontWeight:"600"}}>
+                            Create!
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         );
     }
 }
@@ -21,5 +75,33 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:'center'
+    },
+    title:{
+        fontSize:28,
+        fontWeight:'bold',
+        color:colors.black,
+        alignSelf:'center',
+        marginBottom:16
+    },
+    input:{
+        borderWidth:StyleSheet.hairlineWidth,
+        borderColor:colors.blue,
+        borderRadius:6,
+        height:50,
+        marginTop:8,
+        paddingHorizontal:16,
+        fontSize:18
+    },
+    create:{
+        marginTop:24,
+        height:50,
+        borderRadius:6,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    colorSelect:{
+        width:30,
+        height:30,
+        borderRadius:4
     }
 })
